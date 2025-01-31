fa104545661b1c32fd78badf17322ef55d8a1611
bbaker101 <140855046+bbaker101@users.noreply.github.com>
2025-01-31 08:54:36 -0800

    Kickoff

---
+++ Meth.java
@@ -0,0 +1,11 @@
+public class meth {
+    public static ArrayList<String> substringify(String input){
+        ArrayList<String> ret = new ArrayList<>();
+        //boolean paraEnd = false;
+        //for(int i = input.length; i>= )
+        int 
+        for(int i=0;i<input.length;i++){
+
+        }
+    }
+}
